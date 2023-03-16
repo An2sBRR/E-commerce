130cac17bd341f7e657cfa82d8b6439237cd3619
Le_sept_ou_le_siete <76405539+Le-7@users.noreply.github.com>
2023-03-16 10:03:35 +0100

    tri

--- e-commerce/test/supp_cat.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once '../include/config.php';
-    $id = $_GET['id'];
-    $sqlState = $bdd->prepare('DELETE FROM categorie WHERE id=?');
-    $supprime = $sqlState->execute([$id]);
-    header('location: categorie.php');
-?>
