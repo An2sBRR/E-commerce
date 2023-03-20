8c49cd4cd22885acb8870e330f9f2d8388098e92
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-03-20 10:37:39 +0100

    Delete test directory

--- test/supp_prod.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once '../include/config.php';
-    $id = $_GET['id'];
-    $sqlState = $bdd->prepare('DELETE FROM produit WHERE id=?');
-    $supprime = $sqlState->execute([$id]);
-    header('location: produit.php');
-?>
