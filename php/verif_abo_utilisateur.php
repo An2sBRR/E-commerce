a6fe24333ad2118121bc20a2d9a8ac3eb9db69d6
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-03-25 16:18:41 +0100

    Update verif_abo_utilisateur.php

--- php/verif_abo_utilisateur.php
+++ php/verif_abo_utilisateur.php
@@ -2,8 +2,23 @@
     session_start();
 
     if(isset($_SESSION['user'])){
-        echo "valide";
+        if(getStatut() == "client"){
+            echo "valide";
+        }else{
+            echo getStatut();
+        }
     }else{
         echo "invalide";
+    }
+
+    function getStatut(){
+        require '../include/config.php';
+        if(isset($_SESSION['user'])){
+            $req = "SELECT statut FROM utilisateurs WHERE token='".$_SESSION['user']."'";
+            $stmt = $bdd->prepare($req);
+            $stmt->execute();
+            $data = $stmt->fetch();
+            return $data[statut];
+        }
     }
 ?>
