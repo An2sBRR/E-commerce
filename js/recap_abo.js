46eabba0f766b83dd988c0be43e6902e9238aeb2
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-03-25 15:48:28 +0100

    Update recap_abo.js

--- js/recap_abo.js
+++ js/recap_abo.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
         var xhr = new XMLHttpRequest();
        
         xhr.onreadystatechange = function(){
-            if(xhr.status == 200){
+            if(xhr.status == 200 && xhr.readyState == 4){
                 if(this.responseText == "invalide"){
                     document.getElementById('erreur').innerHTML = "Vous n'êtes pas connecté, veuillez vous connecter <a href=\"/php/connexion.php\">ici</a>";
                 }else if(this.responseText == "valide"){
                     var xhr2 = new XMLHttpRequest();
                     xhr2.onreadystatechange = function(){
-                        if(xhr2.status == 200){
+                        if(xhr2.status == 200 && xhr2.readyState == 4){
                             if(this.responseText=="Envoyer!"){
                                 document.getElementById("tout").innerHTML = "Votre abonnement a bien été pris en compte ! Merci pour votre confiance.";
                                 setTimeout(function (){document.location.href="../index.php";}, 4000);
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     xhr2.open("GET", '/php/envoyermail.php', true); 
                     xhr2.send(null);
                 }
-                
             }
         };
         xhr.open("GET", '/php/verif_abo_utilisateur.php', true); 
