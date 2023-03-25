e300758d8644b5235e85d18d2c539dfc2ba54801
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-03-25 15:43:50 +0100

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,5 +1,5 @@
-<!--Connexion avec la base de données-->
 <?php 
+//Connexion avec la base de données
 /*e-commerce est le nom de ma base de donnée, root -> utilisateur et root -> mp
 attention pour mac le mp est root mais pour les autre le nom reste le meme et mdp =" " 
 */
