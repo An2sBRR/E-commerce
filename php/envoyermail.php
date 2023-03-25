768daf1592d3d5b7413941d808d99275598a2e82
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-03-25 15:46:50 +0100

    Update envoyermail.php
    modification de la bdd quand il est abonnement

--- php/envoyermail.php
+++ php/envoyermail.php
@@ -1,16 +1,8 @@
 <?php
 session_start();
-
-$to = "bergeresar@cy-tech.fr";
-$subject = "Utilisation de mail() avec php";
-$message = "Merci d'avoir pris notre abonnement qui va vous permettre de faire des économies. \nPour toute question, n'hésitez pas à nous contactez via notre site internet.\n\nBonne journée et à bientôt,\nL'équipe de Jeux et Loisirs";
-
-$headers = "Content-Type: text/plain; charset=utf-8\r\n";
-$headers = "From: sarahbergere10@gmail.com\r\n";
-
-if(mail($to, $subject, $message, $headers))
+    require '../include/config.php';
+    $req = "UPDATE utilisateurs SET abonnement = 1 WHERE token='".$_SESSION['user']."'";
+    $stmt = $bdd->prepare($req);
+    $stmt->execute();
     echo "Envoyer!";
-else 
-    echo "Erreur envoi";
-
 ?>
