5c302de16e3b85c65dd4305c668f4336e26e3705
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-03-25 22:42:49 +0100

    Update abonnement.php
    si la personne est déjà abonné elle voit la page d'abonnement mais avec un bouton de désabonnement

--- php/abonnement.php
+++ php/abonnement.php
@@ -15,7 +15,8 @@
 </head>
 
 <body>
-    <div class="description">
+    <?php if(getAbonnement() == 0){
+        echo '<div class="description">
         <p class="head">Notre abonnement</h1>
         <p class="texte">Venez découvrir notre nouvel abonnement qui vous récompensera de votre fidélité avec de nombreux avantages ! </p>
     </div>
@@ -25,10 +26,28 @@
             <li class="couleur">9,99€ par an</li>
             <li>Accès à une réduction de 10% sur votre commande</li>
             <li>Accès anticipé pour nos nouvelles sorties</li>
-            <li>Livraison offerte sans minimum d'achat</li>
+            <li>Livraison offerte sans minimum d\'achat</li>
             <li>Accès annuel</li>
-            <li class="couleur"><button class="button" type="button" onclick="location.href='recap_abo.php'">M'abonner</button></li>
+            <li class="couleur"><button class="button" type="button" onclick="location.href=\'recap_abo.php\'">M\'abonner</button></li>
         </ul>
-    </div>
+    </div>';
+        }else{
+            echo '<div class="description"> <p class="head">Vous êtes abonné à notre service d\'abonnement !</p>
+            <p class="abonnement">Si vous souhaitez vous désabonner, appuyez sur le bouton suivant. <br>(Tout désabonnement est définitif, ainsi si vous souhaitez vous réabonner il faudra repayer le prix annuel)</p></div>
+            <input type="button" class="desabonnement" onclick="location.href=\'fin_abo.php\'" value="Je me désabonne !"></input>' ;
+        }
+        ?>
 </body>
 </html>
+<?php 
+function getAbonnement(){
+    require '../include/config.php';
+        if(isset($_SESSION['user'])){
+            $req = "SELECT abonnement FROM utilisateurs WHERE token='".$_SESSION['user']."'";
+            $stmt = $bdd->prepare($req);
+            $stmt->execute();
+            $data = $stmt->fetchColumn();
+            return $data;
+        }else return 2; //il n'est pas connecte
+}
+?>
