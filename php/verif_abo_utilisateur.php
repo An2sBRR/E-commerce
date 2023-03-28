846b6d6526d39e9a86d4d849790edb3118c4af9a
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-03-28 19:09:55 +0200

    modification requete

--- php/verif_abo_utilisateur.php
+++ php/verif_abo_utilisateur.php
@@ -20,8 +20,8 @@
             $req = "SELECT statut FROM utilisateurs WHERE token='".$_SESSION['user']."'";
             $stmt = $bdd->prepare($req);
             $stmt->execute();
-            $data = $stmt->fetch();
-            return $data[statut];
+            $data = $stmt->fetchColumn();
+            return $data;
         }
     }
 
