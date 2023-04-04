13031265f14d0f9ee5dbc3efa86727acd0b66837
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-04-04 16:39:08 +0200

    Update analyse.php

--- profil/vendeur/analyse.php
+++ profil/vendeur/analyse.php
@@ -66,7 +66,7 @@
                             <a href="profil.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
                                 <span class="d-none d-sm-inline">Profil</span> </a>
                         </li>
-                        <a href="../php/deconnexion.php" class="nav-link px-2 text-truncate">
+                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
                         <i class="bi bi-toggle-off"></i></i>
                             <span class="d-none d-sm-inline">Déconnexion</span>
                         </a>
