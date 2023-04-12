b964a949503355bb4acf2a7b9efbf77cba3218df
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-04-12 23:18:04 +0200

    affichage du prix final avec la livraison

--- js/panier.js
+++ js/panier.js
@@ -34,5 +34,51 @@ function modifier_quantite(id, quantite){
             }
         }
     });
-
 }
+
+//permet de calculer le total avec la livraison 
+var livraison = $("#livraison").val();
+var total;
+if(!$("#abonne").length){ //si la personne n'est pas abonné on va modifier une balise pour afficher le total avec la livraison
+    switch (livraison) {
+        case "standard":
+            livraison = 5;
+            break;
+        case "relais":
+            livraison = 2;
+            break;
+        case "express" :
+            livraison = 8;
+            break;
+        default:
+            livraison = null;
+            break;
+    }
+    total = parseFloat($("#total").text());
+    var result = total+livraison;
+    $(".total_final").text("Total avec livraison : "+result+" €");
+}
+
+
+$("#livraison").change(function(){  //on refait la même chose quand la personne choisit une autre livraison
+    livraison = $(this).val();
+    if(!$("#abonne").length){
+        switch (livraison) {
+            case "standard":
+                livraison = 5;
+                break;
+            case "relais":
+                livraison = 2;
+                break;
+            case "express" :
+                livraison = 8;
+                break;
+            default:
+                livraison = null;
+                break;
+        }
+        total = parseFloat($("#total").text());
+        var result = total+livraison;
+        $(".total_final").text("Total avec livraison : "+result+" €");
+    }
+});
