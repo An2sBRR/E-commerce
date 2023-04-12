4ed62fe04532581ed7f5557729f9ee9c7df71cfb
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2023-04-12 16:04:44 +0200

    regex

--- php/traitement_contact.php
+++ php/traitement_contact.php
@@ -13,6 +13,22 @@
         $demande['date'] = date("d-m-Y  H:i") ;
         $demande['id'] = date("dmYHis") ; #Création d'un id unique par utilisateur
 
+        if(!preg_match("/^[a-zA-Z-' ]*$/",$demande['nom'])){
+            echo "<script language='Javascript'>window.alert('Erreur dans le nom (format non compatible)');</script>";
+            echo "<script language='Javascript'>document.location='/php/contact.php?cat=nom'</script>";
+            exit;
+        }
+        if(!filter_var($demande['email'], FILTER_VALIDATE_EMAIL)){
+            echo "<script language='Javascript'>window.alert('Erreur dans l email (format non compatible ex: lesept@gmail.com)');</script>";
+            echo "<script language='Javascript'>document.location='/php/contact.php?cat=email'</script>";
+            exit;
+        }
+        if(!preg_match("/^[0-9]{10}$/",$demande['tel'])){
+            echo "<script language='Javascript'>window.alert('Erreur dans le champs en rouge (format non compatible, numero a 10 chiffres accolés');</script>";
+            echo "<script language='Javascript'>document.location='/php/contact.php?cat=tel'</script>";
+            exit;
+        }
+
   
         $json = file_get_contents('../data/messages.json') ; #var qui contient notre fichier json sous forme de tableau
         $json = json_decode($json, true) ; #conversion en php
