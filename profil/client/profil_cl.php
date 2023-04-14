e0d0ea81bf5c8603bc63d0d728f29b821d059ae3
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-04-14 20:33:31 +0200

    Update profil_cl.php

--- profil/client/profil_cl.php
+++ profil/client/profil_cl.php
@@ -1,4 +1,6 @@
-
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -15,6 +17,8 @@
     <script src="../js/bootstrap.js"></script>
     <script src="../js/plotly-2.18.2.min.js"></script>
     <script src="../js/graph.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -75,46 +79,50 @@
                     <div class="d-flex justify-content-center">
                         <i id="log-logo1" class="bi bi-person-circle"></i>
                     </div>
-                    <div class="d-flex justify-content-center"><h3>Nom Prénom</h3></div>
+                    <div class="d-flex justify-content-center"><h3>
+                        <?php 
+                            require '../../include/config.php';
+                            $requete = $bdd->prepare('SELECT nom, prenom FROM utilisateurs WHERE token = ?');
+                            $requete->execute([$_SESSION['user']]);
+                            $resultat = $requete->fetch();
+                            echo ucfirst($resultat['prenom'])." ";
+                            echo ucfirst($resultat['nom']);
+                        ?>
+                    </h3></div>
                     <h4>Information personnel :</h4>
+                    <div class="container">
+                        <div class="col-md-12">
+                            <?php 
+                                    if(isset($_GET['err'])){
+                                        $err = htmlspecialchars($_GET['err']);
+                                        switch($err){
+                                            case 'current_password':
+                                                echo "<div class='alert alert-danger'>Le mot de passe actuel est incorrect</div>";
+                                            break;
 
-                    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-        <div class="container">
-            <div class="col-md-12">
-                <?php 
-                        if(isset($_GET['err'])){
-                            $err = htmlspecialchars($_GET['err']);
-                            switch($err){
-                                case 'current_password':
-                                    echo "<div class='alert alert-danger'>Le mot de passe actuel est incorrect</div>";
-                                break;
-
-                                case 'success_password':
-                                    echo "<div class='alert alert-success'>Le mot de passe a bien été modifié ! </div>";
-                                break; 
-                            }
-                        }
-                    ?>
+                                            case 'success_password':
+                                                echo "<div class='alert alert-success'>Le mot de passe a bien été modifié ! </div>";
+                                            break; 
+                                        }
+                                    }
+                                ?>
                        
                         <!-- Button trigger modal -->
-                        <p>Vous avez la possibilité de changer votre mot de passe : <button type="button" data-toggle="modal" data-target="#change_password">Changer mon mot de passe</button></p>
-                </div>
-            </div>
-        </div>    
+                            <p>Vous avez la possibilité de changer votre mot de passe : <button type="button" data-toggle="modal" data-target="#change_password">Changer mon mot de passe</button></p>
+                        </div>
+                    </div>
+                </div>    
      
-        <!-- Modal -->
-        <div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Changer mon mot de passe</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                         </div>
+                <!-- Modal -->
+                <div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Changer mon mot de passe</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
                             <div class="modal-body">
                                 <form action="change_mdp.php" method="POST">
                                     <label for='current_password'>Mot de passe actuel</label>
@@ -135,18 +143,12 @@
                         </div>
                     </div>
                 </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-   
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
-
-                </div>                    
             </main>
         </div>
     </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
