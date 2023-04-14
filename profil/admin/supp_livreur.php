befbb3f1a27d8c6076b452a51d0be3d50e964344
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-04-14 11:35:42 +0200

    Delete profil/admin directory

--- profil/admin/supp_livreur.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once '../../include/config.php';
-    $id = $_GET['id'];
-    $sqlState = $bdd->prepare('DELETE FROM employes WHERE id=?');
-    $supprime = $sqlState->execute([$id]);
-    header('location: vendeur.php');
-?>
