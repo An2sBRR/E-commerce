1183c503d10354ab7200ffb3f4e60fd035f823b6
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-04-17 22:42:52 +0200

    modification header selon statut

--- profil/supp_prod.php
+++ profil/supp_prod.php
@@ -3,5 +3,10 @@
     $id = $_GET['id'];
     $sqlState = $bdd->prepare('DELETE FROM produit WHERE id=?');
     $supprime = $sqlState->execute([$id]);
-    header('location: produit.php');
+
+    if($_SESSION['statut'] === "vendeur"){
+        header('location: ./vendeur/produit.php');
+    }else{
+        header('location: ./admin/produit_ad.php');
+    }
 ?>
