c20a1f511428f05977a17ad7d9a26b6197c9a6fd
sara-molie <125445622+sara-molie@users.noreply.github.com>
2023-04-23 20:16:14 +0200

    ajout d'un message vide si pas de message admin

--- php/gestion_messages.php
+++ php/gestion_messages.php
@@ -19,30 +19,39 @@ $demandes = json_decode(file_get_contents('../data/messages.json'), true);
 <body>
 
 <main>
-<?php foreach ($demandes as $demande) : ?>
-    <section class="message <?php echo $demande['utilisateur']; ?>">
-        <a href="traitement_contact.php?del=<?php echo $demande['id']; ?>" class="action" id="ferme">X</a><br>
-        <header>
-            <div class="user-date">
-                <h2><?php echo $demande['utilisateur']; ?></h2>
-                <p><b>Date : </b><?php echo $demande['date']; ?></p>
+
+<!-- si l'admin na recu aucun message alors une notification apparait --->
+<?php if(empty($demandes)): ?>
+    <div class="message">
+        <p>Pas de nouveau message</p>
+    </div>
+<!-- sinon afficher les messages  --->
+<?php else: ?>
+    <?php foreach ($demandes as $demande) : ?>
+        <section class="message <?php echo $demande['utilisateur']; ?>">
+            <a href="traitement_contact.php?del=<?php echo $demande['id']; ?>" class="action" id="ferme">X</a><br>
+            <header>
+                <div class="user-date">
+                    <h2><?php echo $demande['utilisateur']; ?></h2>
+                    <p><b>Date : </b><?php echo $demande['date']; ?></p>
+                </div>
+            </header>
+            <div class="nom">
+                <h3><?php echo $demande['nom']; ?></h3>
             </div>
-        </header>
-        <div class="nom">
-            <h3><?php echo $demande['nom']; ?></h3>
-        </div>
-        <div class="content">
-            <div class="contenu">
-                <p><b><u>Objet</u> :</b> <?php echo $demande['objet']; ?></p>
-                <p><b>Message :</b> <em><?php echo $demande['message']; ?></em></p>
-                <p><b>Email :</b> <?php echo $demande['email']; ?></p>
+            <div class="content">
+                <div class="contenu">
+                    <p><b><u>Objet</u> :</b> <?php echo $demande['objet']; ?></p>
+                    <p><b>Message :</b> <em><?php echo $demande['message']; ?></em></p>
+                    <p><b>Email :</b> <?php echo $demande['email']; ?></p>
+                </div>
+                <div class="coordonnees">
+                    <p><b>Tel :</b> <?php echo $demande['tel']; ?></p>
+                </div>
             </div>
-            <div class="coordonnees">
-                <p><b>Tel :</b> <?php echo $demande['tel']; ?></p>
-            </div>
-        </div>
-    </section>
-<?php endforeach; ?>
+        </section>
+    <?php endforeach; ?>
+<?php endif; ?>
 
 
 </main>
