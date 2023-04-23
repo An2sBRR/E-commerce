604989e35b2586951e6321d114d493d71bba57b0
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-04-23 22:38:08 +0200

    graphique commission

--- profil/admin/analyse_ad.php
+++ profil/admin/analyse_ad.php
@@ -5,7 +5,7 @@
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,8 +18,7 @@
     <link href="../css/index.css" rel="stylesheet">
     <link href="../css/profilpage.css" rel="stylesheet">
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/plotly-2.18.2.min.js"></script>
-    <script src="../js/graph.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <header class="shadow rounded-3 bg-light" id="header-box" >
@@ -72,7 +71,7 @@
                             <a href="main_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
                                 <span class="d-none d-sm-inline">Profil</span> </a>
                         </li>
-                        <a href="php/deconnexion.php" class="nav-link px-2 text-truncate">
+                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
                         <i class="bi bi-toggle-off"></i></i>
                                 <span class="d-none d-sm-inline">Déconnexion</span>
                         </a>
@@ -80,76 +79,111 @@
                 </div>
             </aside>
 
-            <main class="col overflow-auto h-100 w-100">
+            <main class="col overflow-auto">
+                <?php 
+                  require '../../include/config.php';
+                  $requete = $bdd->prepare("SELECT DATE_FORMAT(date_creation, '%Y-%m') as month, SUM(commission) as commission_sum FROM commande GROUP BY month ORDER BY month");
+                  $requete->execute();
+                  $result = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-<!--2 eme graphique LES VENDEURS --->
+                  // On va stocker des données dans des tableaux pour les utiliser en php
+                  $months = array();
+                  $commissions = array();
+                  foreach ($result as $row) {
+                    
+                    array_push($months, $row['month']);
+                    array_push($commissions, $row['commission_sum']);
+                  }                  
+                ?>
+                <title>Graphique des inscriptions de vendeurs</title>
+                <h4>Votre chiffre d'affaire :</h4>
+                <!-- Création de l'élément canvas qui va contenir le graphe -->
+                <canvas id="graphique1"></canvas>
+                <script>
+                  var balise = document.getElementById('graphique1').getContext('2d');
+                  var graph = new Chart(balise, {
+                      type: 'line',
+                      data: {
+                          labels: <?php echo json_encode($months); ?>,
+                          datasets: [{
+                              label: 'Chiffre d\'affaires en fonction des commissions',
+                              data: <?php echo json_encode($commissions); ?>,
+                              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                              borderColor: 'rgba(54, 162, 235, 1)',
+                              borderWidth: 1
+                          }]
+                      },
+                      options: {
+                          scales: {
+                              yAxes: [{
+                                  ticks: {
+                                      beginAtZero: true
+                                  },
+                                  scaleLabel: {
+                                      display: true,
+                                      labelString: 'Somme des commissions'
+                                  }
+                              }],
+                              xAxes: [{
+                                  scaleLabel: {
+                                      display: true,
+                                      labelString: 'Mois'
+                                  }
+                              }]
+                          }
+                      }
+                  });
+                </script>
+                <br><br>
 
-<?php
-require_once '../../include/config.php'; // On inclut la connexion à la bdd
-
-// On récupère les données de la base de données
-$requete = $bdd->query("SELECT DATE(date_inscription) AS date, COUNT(*) AS total_inscriptions FROM utilisateurs WHERE statut = 'vendeur' GROUP BY DATE(date_inscription)");
-$donnees = $requete->fetchAll(PDO::FETCH_ASSOC);
-
-// On construit les tableaux de labels et de données pour le graphique
-$labels = array();
-$data = array();
-foreach ($donnees as $row) {
-    $labels[] = $row['date'];
-    $data[] = $row['total_inscriptions'];
-}
-?>
-   <title>Graphique des inscriptions de vendeurs</title>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<h4>Nombre de vendeur  :</h4>
-<!-- Création de l'élément canvas qui va contenir le graphe -->
-<canvas id="myChart"></canvas>
-<script>
-    // Récupération de l'élément canvas et initialisation du graphe
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // Définition du type de graphe (barre)
-        type: 'bar',
-        // Définition des données à afficher
-        data: {
-            labels: <?php echo json_encode($labels); ?>, // étiquettes de l'axe x (date inscription)
-            datasets: [{
-                label: 'Nombre d\'inscriptions', // nom de la série de données
-                data: <?php echo json_encode($data); ?>, // valeurs de la série de données
-                backgroundColor: 'rgba(75, 192, 192, 0.2)', // couleur de fond des barres
-                borderColor: 'rgba(75, 192, 192, 1)', // couleur de bordure des barres
-                borderWidth: 2 // largeur de bordure des barres
-            }]
-        },
-        // Options de configuration du graphe
-        options: {
-            scales: {
-                xAxes: [{
-                    ticks: {
-                        beginAtZero: true, // afficher l'axe y à partir de 0
-                        stepSize: 1 // afficher les valeurs de l'axe y de 1 en 1
-                    }
-                }]
-            }
-        }
-    });
-</script>
-
-</script>
-
-</body>
-</html>
-
-          
-
-
+              <!--2 eme graphique --->
+              <?php
+                $requete = $bdd->query("SELECT DATE(date_inscription) AS date, COUNT(*) AS total_inscriptions FROM utilisateurs WHERE statut = 'vendeur' GROUP BY DATE(date_inscription)");
+                $donnees = $requete->fetchAll(PDO::FETCH_ASSOC);
+                // On construit les tableaux de labels et de données pour le graphique
+                $labels = array();
+                $data = array();
+                foreach ($donnees as $row) {
+                    $labels[] = $row['date'];
+                    $data[] = $row['total_inscriptions'];
+                }
+              ?>
+                <title>Graphique des inscriptions de vendeurs</title>
+                <h4>Nombre de vendeur  :</h4>
+                <!-- Création de l'élément canvas qui va contenir le graphe -->
+                <canvas id="graphique2"></canvas>
+                <script>
+                    // Récupération de l'élément canvas et initialisation du graphe
+                    var element = document.getElementById('graphique2').getContext('2d');
+                    var graphique = new Chart(element, {
+                        // Définition du type de graphe (barre)
+                        type: 'bar',
+                        // Définition des données à afficher
+                        data: {
+                            labels: <?php echo json_encode($labels); ?>, // étiquettes de l'axe x (date inscription)
+                            datasets: [{
+                                label: 'Nombre d\'inscriptions', // nom de la série de données
+                                data: <?php echo json_encode($data); ?>, // valeurs de la série de données
+                                backgroundColor: 'rgba(75, 192, 192, 0.2)', // couleur de fond des barres
+                                borderColor: 'rgba(75, 192, 192, 1)', // couleur de bordure des barres
+                                borderWidth: 2 // largeur de bordure des barres
+                            }]
+                        },
+                        // Options de configuration du graphe
+                        options: {
+                            scales: {
+                                xAxes: [{
+                                    ticks: {
+                                        beginAtZero: true, // afficher l'axe y à partir de 0
+                                        stepSize: 1 // afficher les valeurs de l'axe y de 1 en 1
+                                    }
+                                }]
+                            }
+                        }
+                    });
+                </script>
             </main>
         </div>
     </div>
-   
-
 </body>
 </html>
-
-
-  
