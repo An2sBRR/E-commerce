b83f896ace2f30f04f6f505e94d6083410e217b2
An2sBRR <84623865+An2sBRR@users.noreply.github.com>
2023-04-26 11:17:22 +0200

    meilleur affichage + header

--- php/gestion_messages.php
+++ php/gestion_messages.php
@@ -6,17 +6,40 @@ $demandes = json_decode(file_get_contents('../data/messages.json'), true);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="2">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href ="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/contacter.css" />
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pacifico" />
+    <link href="https://fonts.googleapis.com/css2?family=Boogaloo&display=swap" rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-    <title>Test données</title>
+     <!-- INCLUSION ICONS -->
+     <script  type = "module"  src = "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" ></script> 
+    <script  nomodule  src = "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" ></script>
+
+    <title>Gestion des messages</title>
 </head>
 <body>
+
+    <!-- HEADER -->
+    <header class="container-fluid header">
+        <div class="container">
+
+            <!-- LOGO SITE -->
+            <a href="../index.php" class="logo">JeuxVentes.fr</a>
+            
+            <!-- ICONS -->
+            <div class="icons">
+                    <a href='./gestion_messages.php' class='reseauxlog'><ion-icon name=mail-unread-outline></ion-icon> </a>
+                    <a href='../profil/admin/main_ad.php' class='reseauxlog'> <ion-icon name=flask-outline></ion-icon> </a>
+                    <a href='./deconnexion.php' class='reseauxlog'><ion-icon name=power-outline></ion-icon> </a>
+             
+        </div>
+    </header>
+    <!-- FIN DU HEADER -->
+
 
 <main>
 
