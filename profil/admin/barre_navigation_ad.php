bbf9d2b1e63e709843e17bc0bac424fa0bb40c9a
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-15 23:43:14 +0200

    barre de navigation
    pour ne pas faire de répétition de code dans l'admin

---
+++ profil/admin/barre_navigation_ad.php
@@ -0,0 +1,27 @@
+<aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
+    <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
+        <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
+            <li class="my-1"><a href="../../index.php" class="nav-link px-2 text-truncate"><i class="bi bi-house fs-5"></i>
+                <span class="d-none d-sm-inline">Accueil</span></a>
+            </li>
+            <li class="my-1"><a href="analyse_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-graph-up"></i></i>
+                <span class="d-none d-sm-inline">Analyse</span></a>
+            </li>
+            <li class="my-1"><a href="vendeur.php" class="nav-link px-2 text-truncate"><i class="bi bi-layout-text-sidebar-reverse"></i></i>
+                <span class="d-none d-sm-inline">Liste des vendeurs</span></a>
+            </li>
+            <li class="my-1"><a href="produit_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i>
+                <span class="d-none d-sm-inline">Liste des produits</span> </a>
+            </li>
+            <li class="my-1 nav-item"><a href="commande.php" class="nav-link px-2 text-truncate"><i class="bi bi-calendar2-check"></i></i>
+                <span class="d-none d-sm-inline">Commandes</span> </a>
+            </li>
+            <li class="my-1"><a href="main_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
+                <span class="d-none d-sm-inline">Profil</span> </a>
+            </li>
+            <li class="my-1"><a href="deconnexion.php" class="nav-link px-2 text-truncate"><i class="bi bi-toggle-off"></i></i>
+                <span class="d-none d-sm-inline">Déconnexion</span></a>
+            </li>
+        </ul>
+    </div>
+</aside>
