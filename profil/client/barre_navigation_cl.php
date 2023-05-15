8af7beab0516d62452ea2d2fb8973345052395d2
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-16 00:29:45 +0200

    barre de navigation

---
+++ profil/client/barre_navigation_cl.php
@@ -0,0 +1,32 @@
+<aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
+    <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
+        <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
+
+            <li class="my-1">
+                <a href="../../index.php" class="nav-link px-2 text-truncate">
+                    <i class="bi bi-house fs-5"></i>
+                    <span class="d-none d-sm-inline">Accueil</span>
+                </a>
+            </li>
+            <li class="my-1">
+                <a href="profil_cl.php" class="nav-link px-2 text-truncate">
+                    <i class="bi bi-layout-text-sidebar-reverse"></i></i>
+                    <span class="d-none d-sm-inline">Mon profil</span>
+                </a>
+            </li>
+            <li class="my-1 nav-item">
+                <a href="commande.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i>
+                    <span class="d-none d-sm-inline">Commandes</span> </a>
+            </li>
+            <li class="my-1">
+                <a href="politique.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
+                    <span class="d-none d-sm-inline">Politiques</span> </a>
+            </li>
+            <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
+                <i class="bi bi-toggle-off"></i></i>
+                <span class="d-none d-sm-inline">Déconnexion</span>
+            </a>
+        </ul>
+    </div>
+</aside>
+<!-- fin du menu  -->
