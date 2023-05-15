6a2c6486e90871be5d113c5907ccfa67fe490bad
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-16 00:29:20 +0200

    indentation

--- profil/client/facture.php
+++ profil/client/facture.php
@@ -21,8 +21,6 @@ sinon l'utilisateur est redireigé sur la page index -->
     <link href="../css/index.css" rel="stylesheet">
     <link href="../css/profilpage.css" rel="stylesheet">
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/plotly-2.18.2.min.js"></script>
-    <script src="../js/graph.js"></script>
 </head>
 
 <body>
@@ -30,7 +28,7 @@ sinon l'utilisateur est redireigé sur la page index -->
         <div class="container-fluid col-11" id="header-container">
           <div class=" d-flex align-items-center justify-content-between">
             <div class="py-3 col-sm-auto justify-content-center">
-              <div id="title">JeuVente.fr</div>
+              <div id="title">JeuxVente.fr</div>
             </div>
             <div class="dropdown text-end">
               <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
@@ -42,38 +40,7 @@ sinon l'utilisateur est redireigé sur la page index -->
     </header>
     <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
         <div class="row flex-grow-sm-1 flex-grow-0 container-fluid">
-            <aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
-                <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
-                    <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
-
-                        <li class="my-1">
-                            <a href="../../index.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-house fs-5"></i>
-                                <span class="d-none d-sm-inline">Accueil</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="profil_cl.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-layout-text-sidebar-reverse"></i></i>
-                                <span class="d-none d-sm-inline">Mon profil</span>
-                            </a>
-                        </li>
-                        <li class="my-1 nav-item">
-                            <a href="commande.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i>
-                                <span class="d-none d-sm-inline">Commandes</span> </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="politique.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
-                                <span class="d-none d-sm-inline">Politiques</span> </a>
-                        </li>
-                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
-                        <i class="bi bi-toggle-off"></i></i>
-                                <span class="d-none d-sm-inline">Déconnexion</span>
-                        </a>
-                    </ul>
-                </div>
-            </aside>
-<!-- fin du menu  -->
+            <?php include 'barre_navigation_cl.php'; ?>
             <main class="col overflow-auto h-100 w-100">
             <div class="container py-2">
             <table class="table table-striped table-hover">
@@ -134,9 +101,9 @@ sinon l'utilisateur est redireigé sur la page index -->
     </div>
 <!-- SCRIPT QUI PERMET D'IMPRIMER LA FACTURE ---> 
     <script type="text/javascript">
-function imprimer_page(){
-  window.print();
-}
-</script>
+        function imprimer_page(){
+        window.print();
+        }
+    </script>
 </body>
 </html>
