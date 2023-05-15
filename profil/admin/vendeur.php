0ddf818931c7dc625cecef885a7cb5b764fc2684
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-15 23:41:35 +0200

    indentation + barre de navigation

--- profil/admin/vendeur.php
+++ profil/admin/vendeur.php
@@ -6,72 +6,84 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-<head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Votre Profil</title><link href="../css/bootstrap.css" rel="stylesheet"><link href="../css/bootstrap-icons.css" rel="stylesheet"><link href="../css/sidenav.css" rel="stylesheet"><link href="../css/header2.css" rel="stylesheet"><link href="../css/index.css" rel="stylesheet"><link href="../css/profilpage.css" rel="stylesheet"><script src="../js/bootstrap.js"></script><script src="../js/plotly-2.18.2.min.js"></script><script src="../js/graph.js"></script>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste vendeurs</title>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/bootstrap-icons.css" rel="stylesheet">
+    <link href="../css/sidenav.css" rel="stylesheet">
+    <link href="../css/header2.css" rel="stylesheet">
+    <link href="../css/index.css" rel="stylesheet">
+    <link href="../css/profilpage.css" rel="stylesheet">
+    <script src="../js/bootstrap.js"></script>
 </head>
 
 <body>
-    <header class="shadow rounded-3 bg-light" id="header-box" >
+    <header class="shadow rounded-3 bg-light" id="header-box">
         <div class="container-fluid col-11" id="header-container">
-          <div class=" d-flex align-items-center justify-content-between">
-            <div class="py-3 col-sm-auto justify-content-center">
-              <div id="title">JeuVente.fr</div>
+            <div class=" d-flex align-items-center justify-content-between">
+                <div class="py-3 col-sm-auto justify-content-center">
+                    <div id="title">JeuxVente.fr</div>
+                </div>
+                <div class="dropdown text-end"><a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i id="log-logo" class="bi bi-person-circle"></i></a></div>
             </div>
-            <div class="dropdown text-end"><a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i id="log-logo" class="bi bi-person-circle"></i></a></div></div></div></header><div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto"><div class="row flex-grow-sm-1 flex-grow-0 container-fluid"><aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2"><div class="bg-light border rounded-3 p-1 h-100 sticky-top"><ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate"><li class="my-1"><a href="../../index.php" class="nav-link px-2 text-truncate"><i class="bi bi-house fs-5"></i><span class="d-none d-sm-inline">Accueil</span></a></li><li class="my-1"><a href="analyse_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-graph-up"></i></i><span class="d-none d-sm-inline">Analyse</span></a></li><li class="my-1"><a href="vendeur.php" class="nav-link px-2 text-truncate"><i class="bi bi-layout-text-sidebar-reverse"></i></i><span class="d-none d-sm-inline">Liste des vendeurs</span></a></li><li class="my-1"><a href="produit_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i><span class="d-none d-sm-inline">Liste des produits</span> </a></li><li class="my-1 nav-item"><a href="commande.php" class="nav-link px-2 text-truncate"><i class="bi bi-calendar2-check"></i></i><span class="d-none d-sm-inline">Commandes</span> </a></li><li class="my-1"><a href="main_ad.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i><span class="d-none d-sm-inline">Profil</span> </a></li><a href="deconnexion.php" class="nav-link px-2 text-truncate"><i class="bi bi-toggle-off"></i></i><span class="d-none d-sm-inline">Déconnexion</span></a></ul></div>
-</aside>
-
+        </div>
+    </header>
+    <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
+        <div class="row flex-grow-sm-1 flex-grow-0 container-fluid">
+            <!-- Barre de navigation -->
+            <?php include 'barre_navigation_ad.php'; ?>
+            
             <main class="col overflow-auto h-100 w-100">
                 <div class="container py-2">
-                <h2>Liste des vendeurs</h2>
-<!-- Création d'une table pour afficher les vendeurs -->
-<table class="table table-striped table-hover">
-    <thead>
-        <tr>
-            <!-- Création de l'en-tête du tableau -->
-            <th>#ID</th>
-            <th>Nom</th>
-            <th>Prenom</th>
-            <th>Pseudo</th>
-            <th>Ville</th>
-            <th>Opérations</th>
-        </tr>
-    </thead>
+                    <h2>Liste des vendeurs</h2>
+                    <!-- Création d'une table pour afficher les vendeurs -->
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <!-- Création de l'en-tête du tableau -->
+                                <th>#ID</th>
+                                <th>Nom</th>
+                                <th>Prenom</th>
+                                <th>Pseudo</th>
+                                <th>Ville</th>
+                                <th>Opérations</th>
+                            </tr>
+                        </thead>
 
-    <?php
-// Connexion à la base de données
-require_once '../../include/config.php';
+                        <?php
+                            // Connexion à la base de données
+                            require_once '../../include/config.php';
 
-// Exécution d'une requête SQL pour récupérer toutes les informations des utilisateurs
-$req = $bdd->query("SELECT * FROM utilisateurs");
+                            // Exécution d'une requête SQL pour récupérer toutes les informations des utilisateurs
+                            $req = $bdd->query("SELECT * FROM utilisateurs");
 
-// Vérification si la requête a retourné des résultats
-if ($req->rowCount() > 0) {
-    // Parcours de chaque utilisateur retourné par la requête
-    while($utilisateurs = $req->fetch()) {
-        // Vérification si l'utilisateur est un vendeur
-        if($utilisateurs["statut"] == "vendeur"){
-            // Affichage des informations du vendeur dans le tableau
-            echo "<tr>
-                    <td>".$utilisateurs["id"]."</td>
-                    <td>".$utilisateurs["nom"]."</td>
-                    <td>".$utilisateurs["prenom"]."</td>
-                    <td>".$utilisateurs["pseudo"]."</td>
-                    <td>".$utilisateurs["ville"]."</td>
+                            // Vérification si la requête a retourné des résultats
+                            if ($req->rowCount() > 0) {
+                                // Parcours de chaque utilisateur retourné par la requête
+                                while($utilisateurs = $req->fetch()) {
+                                    // Vérification si l'utilisateur est un vendeur
+                                    if($utilisateurs["statut"] == "vendeur"){
+                                        // Affichage des informations du vendeur dans le tableau
+                                        echo "<tr>
+                                                <td>".$utilisateurs["id"]."</td>
+                                                <td>".$utilisateurs["nom"]."</td>
+                                                <td>".$utilisateurs["prenom"]."</td>
+                                                <td>".$utilisateurs["pseudo"]."</td>
+                                                <td>".$utilisateurs["ville"]."</td>
 
-                    <td>
-                        <!-- Bouton pour supprimer le vendeur -->
-                        <a  class='btn btn-primary' href='supp_vendeur.php?id=".$utilisateurs["id"]."' onclick=\"return confirm('Êtes-vous sûr de vouloir supprimer ce vendeur?');\">Supprimer</a>
-                    </td>
-                  </tr>";
-        }
-    }
-}
-?>
-                      
- 
-
-                    
-        
-                        
+                                                <td>
+                                                    <!-- Bouton pour supprimer le vendeur -->
+                                                    <a  class='btn btn-primary' href='supp_vendeur.php?id=".$utilisateurs["id"]."' onclick=\"return confirm('Êtes-vous sûr de vouloir supprimer ce vendeur?');\">Supprimer</a>
+                                                </td>
+                                            </tr>";
+                                    }
+                                }
+                            }
+                        ?>
                     </table>
                 </div>
             </main>
