c4ee8c601aaf2785b0b0b5c05f2bc2fd53d065a0
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 00:33:55 +0200

    Update afficher_com_lv.php

--- profil/livreur/afficher_com_lv.php
+++ profil/livreur/afficher_com_lv.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Détail Colis</title>
+    <title>Détails Colis</title>
     <link href="../css/bootstrap.css" rel="stylesheet">
     <link href="../css/bootstrap-icons.css" rel="stylesheet">
     <link href="../css/sidenav.css" rel="stylesheet">
