81e261773a15d5a8e73be703a71ae667b9a17be9
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 00:31:12 +0200

    changement bouton

--- profil/livreur/afficher_com_lv.php
+++ profil/livreur/afficher_com_lv.php
@@ -6,11 +6,12 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil livreur</title>
+    <title>Détail Colis</title>
     <link href="../css/bootstrap.css" rel="stylesheet">
     <link href="../css/bootstrap-icons.css" rel="stylesheet">
     <link href="../css/sidenav.css" rel="stylesheet">
@@ -22,19 +23,21 @@
     <script src="../js/graph.js"></script>
 </head>
 <!-----EN TETE // MENU -------->
+
 <body>
-    <header class="shadow rounded-3 bg-light" id="header-box" >
+    <header class="shadow rounded-3 bg-light" id="header-box">
         <div class="container-fluid col-11" id="header-container">
-          <div class=" d-flex align-items-center justify-content-between">
-            <div class="py-3 col-sm-auto justify-content-center">
-              <div id="title">JeuVente.fr</div>
+            <div class=" d-flex align-items-center justify-content-between">
+                <div class="py-3 col-sm-auto justify-content-center">
+                    <div id="title">JeuxVente.fr</div>
+                </div>
+                <div class="dropdown text-end">
+                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i id="log-logo" class="bi bi-person-circle"></i>
+                    </a>
+                </div>
             </div>
-            <div class="dropdown text-end">
-              <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <i id="log-logo" class="bi bi-person-circle"></i>
-              </a> 
-            </div>
-          </div>
         </div>
     </header>
     <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
@@ -50,12 +53,13 @@
                         </li>
                         <li class="my-1 nav-item">
                             <a href="colis.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-layout-text-sidebar-reverse"></i></i>
+                                <i class="bi bi-layout-text-sidebar-reverse"></i></i>
                                 <span class="d-none d-sm-inline">Colis</span>
                             </a>
                         </li>
                         <li class="my-1">
-                            <a href="testmap.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i>
+                            <a href="testmap.php" class="nav-link px-2 text-truncate"><i
+                                    class="bi bi-card-text fs-5"></i>
                                 <span class="d-none d-sm-inline">Planning livraison</span> </a>
                         </li>
                         <li class="my-1">
@@ -63,69 +67,81 @@
                                 <span class="d-none d-sm-inline">Profil</span> </a>
                         </li>
                         <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
-                        <i class="bi bi-toggle-off"></i></i>
-                                <span class="d-none d-sm-inline">Déconnexion</span>
+                            <i class="bi bi-toggle-off"></i></i>
+                            <span class="d-none d-sm-inline">Déconnexion</span>
                         </a>
                     </ul>
                 </div>
             </aside>
 
-<!------------FIN MENU ---------------------->
+            <!------------FIN MENU ---------------------->
             <main class="col overflow-auto h-100 w-100">
                 <div class="container py-2">
-            <?php
-  //connexion à la base de données
-  require_once '../../include/config.php'; 
-$idCommande = $_GET['id'];
-$sqlState = $bdd->prepare('SELECT commande.*,utilisateurs.pseudo as "pseudo" FROM commande 
-            INNER JOIN utilisateurs ON commande.id_client = utilisateurs.id 
-                                               WHERE commande.id = ?
-                                               ORDER BY commande.date_creation DESC');
-$sqlState->execute([$idCommande]);
-$commande = $sqlState->fetch(PDO::FETCH_ASSOC);
+                    <?php
+                        //connexion à la base de données
+                        require_once '../../include/config.php'; 
+                        $idCommande = $_GET['id'];
+                        $sqlState = $bdd->prepare('SELECT commande.*,utilisateurs.pseudo as "pseudo" FROM commande 
+                                    INNER JOIN utilisateurs ON commande.id_client = utilisateurs.id 
+                                                                    WHERE commande.id = ?
+                                                                    ORDER BY commande.date_creation DESC');
+                        $sqlState->execute([$idCommande]);
+                        $commande = $sqlState->fetch(PDO::FETCH_ASSOC);
 
-?>
-    <h2>Détails du colis</h2>
-    <table class="table table-striped table-hover">
- <!------------on fait un tableau qui va permettre d'afficher la liste de nos colis---------------------->
-        <thead>
-        <tr>
-            <th>#ID</th><th>Client</th><th>Numéro commande</th><th>taille</th><th>poids</th><th>adresse livraison </th><th>ville</th><th>code postal </th><th>Date</th><th>Opérations</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        //requete sql qui permet de prendre toutes les informations dans la table ligne_commande et produit ce qui va permettre de detailler les commandes
-        $sqlStateLigne_commande = $bdd->prepare('SELECT ligne_commande.*,produit.libelle,produit.image from ligne_commande
-                                                        INNER JOIN produit ON ligne_commande.id_produit = produit.id
-                                                        WHERE id_commande = ?
-                                                        ');
-        $sqlStateLigne_commande->execute([$idCommande]);
-        $lignesCommandes = $sqlStateLigne_commande->fetchAll(PDO::FETCH_OBJ);
-        ?>
-        <tr>
-            <td><?php echo $commande['id'] ?></td>
-            <td><?php echo $commande['nom_prenom'] ?></td>
-            <td><?php echo $commande['numero_commande'] ?></td>
-            <td><?php echo $commande['taille'] ?></td>
-            <td><?php echo $commande['poids'] ?></td>
-            <td><?php echo $commande['adresse_livraison'] ?></td>
-            <td><?php echo $commande['ville'] ?></td>
-            <td><?php echo $commande['code_postal'] ?></td>
-            <td><?php echo $commande['date_creation'] ?></td>
-            <td>
-                <!------------on regarde l'etat de la commande, si l'etat est validé alors on affiche livré---------------------->
-                <?php if ($commande['commande_livre'] == 0) : ?>
-                    <a class="btn btn-success btn-sm" href="commande_livre.php?id=<?= $commande['id']?>&etat=1">Colis livré</a>
-                    <?php else: ?>
-                    <a class="btn btn-danger btn-sm" href="commande_livre.php?id=<?= $commande['id']?>&etat=0">Colis en cours de livraison</a>
-                <?php endif; ?>
-            </td>
-        </tr>
-        <?php
-        ?>
-        </tbody>
-    </table>
-
+                    ?>
+                    <h2>Détails du colis</h2>
+                    <table class="table table-striped table-hover">
+                        <!------------on fait un tableau qui va permettre d'afficher la liste de nos colis---------------------->
+                        <thead>
+                            <tr>
+                                <th>#ID</th>
+                                <th>Client</th>
+                                <th>Numéro commande</th>
+                                <th>taille</th>
+                                <th>poids</th>
+                                <th>adresse livraison </th>
+                                <th>ville</th>
+                                <th>code postal </th>
+                                <th>Date</th>
+                                <th>Etat du colis</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                //requete sql qui permet de prendre toutes les informations dans la table ligne_commande et produit ce qui va permettre de detailler les commandes
+                                $sqlStateLigne_commande = $bdd->prepare('SELECT ligne_commande.*,produit.libelle,produit.image from ligne_commande
+                                                                                INNER JOIN produit ON ligne_commande.id_produit = produit.id
+                                                                                WHERE id_commande = ?
+                                                                                ');
+                                $sqlStateLigne_commande->execute([$idCommande]);
+                                $lignesCommandes = $sqlStateLigne_commande->fetchAll(PDO::FETCH_OBJ);
+                            ?>
+                            <tr>
+                                <td><?php echo $commande['id'] ?></td>
+                                <td><?php echo $commande['nom_prenom'] ?></td>
+                                <td><?php echo $commande['numero_commande'] ?></td>
+                                <td><?php echo $commande['taille'] ?></td>
+                                <td><?php echo $commande['poids'] ?></td>
+                                <td><?php echo $commande['adresse_livraison'] ?></td>
+                                <td><?php echo $commande['ville'] ?></td>
+                                <td><?php echo $commande['code_postal'] ?></td>
+                                <td><?php echo $commande['date_creation'] ?></td>
+                                <td>
+                                    <!------------on regarde l'etat de la commande, si l'etat est validé alors on affiche livré---------------------->
+                                    <?php if ($commande['commande_livre'] == 0) : ?>
+                                    <a class="btn btn-danger btn-sm"
+                                        href="commande_livre.php?id=<?= $commande['id']?>&etat=1">Colis en cours de livraison</a>
+                                    <?php else: ?>
+                                    <a class="btn btn-success btn-sm"
+                                        href="commande_livre.php?id=<?= $commande['id']?>&etat=0">Colis livré</a>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </main>
+        </div>
+    </div>
 </body>
 </html>
