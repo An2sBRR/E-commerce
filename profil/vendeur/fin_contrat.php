bf58e7c266819aaa431424d793c42297295b4fe7
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 02:41:45 +0200

    Add files via upload

---
+++ profil/vendeur/fin_contrat.php
@@ -0,0 +1,22 @@
+<?php
+    require '../../include/config.php';
+
+    if (isset($_SESSION['user'])) {
+        $requete = $bdd->prepare('SELECT date_inscription FROM utilisateurs WHERE token = ?');
+        $requete->execute([$_SESSION['user']]);
+        $utilisateur = $requete->fetch();
+
+        if ($utilisateur) {
+            $date_inscription = new DateTime($utilisateur['date_inscription']);
+            $date_inscription->modify('+1 year');
+            $nouvelle_date = $date_inscription->format('Y-m-d H:i:s');
+            $date_today = date('Y-m-d H:i:s');
+
+            if ($nouvelle_date < $date_today) {
+                echo '<script type="text/javascript">';
+                echo 'alert("Votre contrat a pris fin.")';
+                echo '</script>';
+            }
+        }
+    }
+?>
