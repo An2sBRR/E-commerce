6ca0c251d41f31e97b4f16f522a310d32c5beef3
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 12:26:01 +0200

    indentation

--- profil/livreur/commande_livre.php
+++ profil/livreur/commande_livre.php
@@ -3,16 +3,16 @@
     if(!isset($_SESSION['user']) || $_SESSION['statut'] != "livreur"){
         header('Location: ../../index.php');
     }
-?>
-<!------------cette page permet de validé les commandes, ce qui permet de voir l'acheminement coté client--------------------->
 
-<?php
-//on inclus la bdd
-include_once '../../include/config.php'; 
-//on prends l'id et l'etat de la commande
-$id = $_GET['id'];
-$etat = $_GET['etat'];
-//on met la table commande a jour ce qui permet de modifier son etat 
-$sqlState = $bdd->prepare('UPDATE commande SET commande_livre = ? WHERE id = ?');
-$sqlState->execute([$etat, $id]);
-header('location: afficher_com_lv.php?id=' . $id);
+//------------cette page permet de validé les commandes, ce qui permet de voir l'acheminement coté client--------------------->
+
+    //on inclus la bdd
+    include_once '../../include/config.php'; 
+    //on prends l'id et l'etat de la commande
+    $id = $_GET['id'];
+    $etat = $_GET['etat'];
+    //on met la table commande a jour ce qui permet de modifier son etat 
+    $sqlState = $bdd->prepare('UPDATE commande SET commande_livre = ? WHERE id = ?');
+    $sqlState->execute([$etat, $id]);
+    header('location: afficher_com_lv.php?id=' . $id);
+?>
