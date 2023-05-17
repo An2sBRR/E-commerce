6ba38ee8fce8a551ef763c7903a0aff7b4b40299
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 12:20:27 +0200

    barre de  navigation

--- profil/livreur/afficher_com_lv.php
+++ profil/livreur/afficher_com_lv.php
@@ -19,8 +19,6 @@
     <link href="../css/index.css" rel="stylesheet">
     <link href="../css/profilpage.css" rel="stylesheet">
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/plotly-2.18.2.min.js"></script>
-    <script src="../js/graph.js"></script>
 </head>
 <!-----EN TETE // MENU -------->
 
@@ -31,48 +29,18 @@
                 <div class="py-3 col-sm-auto justify-content-center">
                     <div id="title">JeuxVente.fr</div>
                 </div>
-                <div class="dropdown text-end">
-                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                <div class="text-end">
+                    <a href="profil2.php" class="d-block link-dark text-decoration-none">
                         <i id="log-logo" class="bi bi-person-circle"></i>
-                    </a>
+                    </a> 
                 </div>
             </div>
         </div>
     </header>
     <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
         <div class="row flex-grow-sm-1 flex-grow-0 container-fluid">
-            <aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
-                <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
-                    <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
-                        <li class="my-1">
-                            <a href="../../index.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-house fs-5"></i>
-                                <span class="d-none d-sm-inline">Accueil</span>
-                            </a>
-                        </li>
-                        <li class="my-1 nav-item">
-                            <a href="colis.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-layout-text-sidebar-reverse"></i></i>
-                                <span class="d-none d-sm-inline">Colis</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="testmap.php" class="nav-link px-2 text-truncate"><i
-                                    class="bi bi-card-text fs-5"></i>
-                                <span class="d-none d-sm-inline">Planning livraison</span> </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="profil2.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
-                                <span class="d-none d-sm-inline">Profil</span> </a>
-                        </li>
-                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-toggle-off"></i></i>
-                            <span class="d-none d-sm-inline">Déconnexion</span>
-                        </a>
-                    </ul>
-                </div>
-            </aside>
+            <!-- BARRE DE NAVIGATION -->
+            <?php include 'barre_navigation_lv.php'; ?>
 
             <!------------FIN MENU ---------------------->
             <main class="col overflow-auto h-100 w-100">
