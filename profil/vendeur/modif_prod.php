b59579a617af004b1d1efbb4c367a8ae8863c532
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 02:45:35 +0200

    Update modif_prod.php

--- profil/vendeur/modif_prod.php
+++ profil/vendeur/modif_prod.php
@@ -3,7 +3,10 @@
     if(!isset($_SESSION['user']) || $_SESSION['statut'] != "vendeur"){
         header('Location: ../../index.php');
     }
-?><!DOCTYPE html>
+
+    include 'fin_contrat.php';
+?>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -17,8 +20,6 @@
     <link href="../css/index.css" rel="stylesheet">
     <link href="../css/profilpage.css" rel="stylesheet">
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/plotly-2.18.2.min.js"></script>
-    <script src="../js/graph.js"></script>
 </head>
 
 <body>
@@ -26,10 +27,10 @@
         <div class="container-fluid col-11" id="header-container">
           <div class=" d-flex align-items-center justify-content-between">
             <div class="py-3 col-sm-auto justify-content-center">
-              <div id="title">JeuVente.fr</div>
+              <div id="title">JeuxVente.fr</div>
             </div>
-            <div class="dropdown text-end">
-              <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <div class="text-end">
+              <a href="profil.php" class="d-block link-dark text-decoration-none">
                 <i id="log-logo" class="bi bi-person-circle"></i>
               </a> 
             </div>
@@ -38,48 +39,16 @@
       </header>
       <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
         <div class="row flex-grow-sm-1 flex-grow-0 container-fluid">
-            <aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
-                <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
-                    <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
-                        <li class="my-1">
-                            <a href="../../index.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-house fs-5"></i>
-                                <span class="d-none d-sm-inline">Accueil</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="categorie.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-graph-up"></i></i>
-                                <span class="d-none d-sm-inline">Analyse</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="categorie.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-layout-text-sidebar-reverse"></i></i>
-                                <span class="d-none d-sm-inline">Liste des categories</span>
-                            </a>
-                        </li>
-                        <li class="my-1 nav-item">
-                            <a href="produit.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i>
-                                <span class="d-none d-sm-inline">Liste des produits</span> </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="profil.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
-                                <span class="d-none d-sm-inline">Profil</span> </a>
-                        </li>
-                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
-                        <i class="bi bi-toggle-off"></i></i>
-                            <span class="d-none d-sm-inline">Déconnexion</span>
-                        </a>
-                    </ul>
-                </div>
-            </aside>
+            <!-- BARRE DE NAVIGATION -->
+            <?php include 'barre_navigation_vd.php'; ?>
             <main class="col overflow-auto h-100 w-100">
                 <?php
                 require'../../include/config.php';
                 ?>
                     <h4>Modifier produit</h4>
                     <?php
+
+                    //récupération des données
                     $id = $_GET['id'];
                     $sqlState = $bdd->prepare('SELECT * from produit WHERE id=?');
                     $sqlState->execute([$id]);
@@ -101,9 +70,8 @@
                             move_uploaded_file($_FILES['image']['tmp_name'], '../../data/' . $filename);
                         }
 
-
                         if (!empty($libelle) && !empty($prix) && !empty($categorie) && !(empty($hauteur)) && !(empty($quantite)) && !(empty($poids))) {
-
+                            //On met à jour la base de donnée avec les nouvelles données récupérés
                             if (!empty($filename)) {
                                 $query = "UPDATE produit SET libelle=? ,
                                                                     prix=? ,
@@ -133,7 +101,7 @@
                             }
 
                             if ($updated) {
-                                //header('location: produit.php');
+                                header('location: produit.php');
                             } else {
 
                                 ?>
@@ -145,7 +113,7 @@
                         } else {
                             ?>
                             <div class="alert alert-danger" role="alert">
-                                Libelle , prix , catégorie sont obligatoires.
+                                Le libellé, le prix et le nom du produits sont obligatoires.
                             </div>
                             <?php
                         }
