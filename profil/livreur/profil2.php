6f7e033f5de9569992eb4fc5dc879881adf1548e
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 12:37:56 +0200

    barre de navigation

--- profil/livreur/profil2.php
+++ profil/livreur/profil2.php
@@ -19,8 +19,6 @@
     <link href="../css/index.css" rel="stylesheet">
     <link href="../css/profilpage.css" rel="stylesheet">
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/plotly-2.18.2.min.js"></script>
-    <script src="../js/graph.js"></script>
 </head>
 
 <body>
@@ -28,52 +26,22 @@
         <div class="container-fluid col-11" id="header-container">
             <div class=" d-flex align-items-center justify-content-between">
                 <div class="py-3 col-sm-auto justify-content-center">
-                    <div id="title">JeuVente.fr</div>
+                    <div id="title">JeuxVente.fr</div>
                 </div>
-                <div class="dropdown text-end">
-                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                <div class="text-end">
+                    <a href="profil2.php" class="d-block link-dark text-decoration-none">
                         <i id="log-logo" class="bi bi-person-circle"></i>
-                    </a>
+                    </a> 
                 </div>
             </div>
         </div>
     </header>
     <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
         <div class="row flex-grow-sm-1 flex-grow-0 container-fluid">
-            <aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
-                <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
-                    <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
+            <!-- BARRE DE NAVIGATION -->
+            <?php include 'barre_navigation_lv.php'; ?>
 
-                        <li class="my-1 ">
-                            <a href="index2.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-house fs-5"></i>
-                                <span class="d-none d-sm-inline">Accueil</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="colis.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-layout-text-sidebar-reverse"></i></i>
-                                <span class="d-none d-sm-inline">Colis</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="testmap.php" class="nav-link px-2 text-truncate"><i
-                                    class="bi bi-card-text fs-5"></i>
-                                <span class="d-none d-sm-inline">Planning livraison</span> </a>
-                        </li>
-                        <li class="my-1 nav-item">
-                            <a href="#" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
-                                <span class="d-none d-sm-inline">Profil</span> </a>
-                        </li>
-                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-toggle-off"></i></i>
-                            <span class="d-none d-sm-inline">Déconnexion</span>
-                        </a>
-                    </ul>
-                </div>
-            </aside>
-<!------------FIN MENU ---------------------->
+            <!------------FIN MENU ---------------------->
 
             <main class="col overflow-auto h-100 w-100">
                 <div class="bg-light border rounded-3 p-3">
@@ -131,7 +99,7 @@
                                 <div class="#">
                                     <div class="row">
                                         <div class="col">
-                                              <select name="type_permis" class="form-select" aria-label="Default select example" required>
+                                            <select name="type_permis" class="form-select" aria-label="Default select example" required>
                                                 <option disabled>--Type de permis--</option>
                                                 <option value="1" >Permis cyclomoteur : AM</option>
                                                 <option value="2">Permis auto : B</option>
@@ -142,7 +110,7 @@
                                         <div class="col">
                                             <label for="horaire_debut">Veuillez choisir une heure de début :</label>
                                             <input id="horaire_debut" type="time" name="horaire_debut" min="06:00"
-                                                max="12:00" required value="7:30">
+                                                max="12:00" required value="07:30">
                                         </div>
                                         <div class="col">
                                             <label for="horaire_fin">Veuillez choisir une heure de fin :</label>
@@ -154,65 +122,64 @@
                             </div>
                             <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41849.53154480777!2d2.0043335859188436!3d49.03729566715635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6f4c72416d693%3A0x40b82c3688b34e0!2sCergy!5e0!3m2!1sfr!2sfr!4v1679224313614!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
                             <br><br>
-                            <p class="text-danger">Une fois sauvegardées, ces informations ne pourront être modifié.</p>
+                            <p class="text-danger">Une fois sauvegardées, ces informations ne pourront être modifiées.</p>
                             <button type="submit" class="button button1 w-25">Sauvegarder</button>
                         </form>
                             </div>
 
                     </div>
-                   <?php }?>
+                   <?php }else {
 
+                        // Récupération des données du livreur à partir de la base de données
+                        $requete = $bdd->prepare('SELECT type_permis FROM livreur');
+                        $requete->execute(array('id_livreur' => $_SESSION['user']));
+                        $livreur = $requete->fetch();
 
-                
-<?php 
-// Récupération des données du livreur à partir de la base de données
-$requete = $bdd->prepare('SELECT type_permis FROM livreur');
-$requete->execute(array('id_livreur' => $_SESSION['user']));
-$livreur = $requete->fetch();
-
-// Vérification que la requête a renvoyé des résultats
-if ($livreur !== false) {
-    // Détermination du type de permis à partir de la valeur dans la base de données
-    if ($livreur['type_permis'] == 1) {
-        $type_permis = "Permis cyclomoteur : AM"; 
-    } else if ($livreur['type_permis'] == 2) {
-        $type_permis = "Permis auto : B"; 
-    } else if ($livreur['type_permis'] == 3) {
-        $type_permis = "Permis moto : A"; 
-    } else if ($livreur['type_permis'] == 4) {
-        $type_permis = "Permis professionnel : C et D"; 
-    } else {
-        $type_permis = "Type de permis inconnu";
-    }
-} 
-?>
-<!-- Affichage du type de permis et des horaire -->
-<div class="container">
-    <div class="col-md-12">
-        <p> Type de permis : <strong><?php 
-            echo ucfirst($type_permis). " ";
-        ?></strong></p>
-    </div>
-</div>
+                        // Vérification que la requête a renvoyé des résultats
+                        if ($livreur !== false) {
+                            // Détermination du type de permis à partir de la valeur dans la base de données
+                            if ($livreur['type_permis'] == 1) {
+                                $type_permis = "Permis cyclomoteur : AM"; 
+                            } else if ($livreur['type_permis'] == 2) {
+                                $type_permis = "Permis auto : B"; 
+                            } else if ($livreur['type_permis'] == 3) {
+                                $type_permis = "Permis moto : A"; 
+                            } else if ($livreur['type_permis'] == 4) {
+                                $type_permis = "Permis professionnel : C et D"; 
+                            } else {
+                                $type_permis = "Type de permis inconnu";
+                            }
+                        } 
+                        ?>
+                    <!-- Affichage du type de permis et des horaire -->
+                    <div class="container">
+                        <div class="col-md-12">
+                            <p> Type de permis : <strong><?php 
+                                echo ucfirst($type_permis). " ";
+                            ?></strong></p>
+                        </div>
+                    </div>
                    <div class="container">
                         <div class="col-md-12">
-                            <p> horaire debut : <strong><?php 
+                            <p> Horaire debut : <strong><?php 
                                 $requete = $bdd->prepare('SELECT horaire_debut FROM livreur');
                                 $requete->execute([$_SESSION['user']]);
                                 $resultat = $requete->fetch();
                                 echo ucfirst($resultat['horaire_debut'])." ";
                             ?></strong></p>
-                </div>
-                <div class="container">
+                        </div>
+                    </div>
+                    <div class="container">
                         <div class="col-md-12">
-                            <p> horaire fin  : <strong><?php 
+                            <p> Horaire fin  : <strong><?php 
                                 $requete = $bdd->prepare('SELECT horaire_fin FROM livreur');
                                 $requete->execute([$_SESSION['user']]);
                                 $resultat = $requete->fetch();
                                 echo ucfirst($resultat['horaire_fin'])." ";
                             ?></strong></p>
-                </div>
-               
+                        </div>
+                    </div>
+                <?php } ?>
                 <hr/>
             </main>
         </div>
