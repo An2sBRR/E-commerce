4985cec3ae05190a9b6577f204f95171c1b05c21
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-17 02:38:28 +0200

    Update main.php

--- profil/vendeur/main.php
+++ profil/vendeur/main.php
@@ -3,6 +3,7 @@
     if(!isset($_SESSION['user']) || $_SESSION['statut'] != "vendeur"){
         header('Location: ../../index.php');
     }
+    include 'fin_contrat.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -10,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un vendeur</title>
+    <title>Espace Vendeur</title>
     <link href="../css/bootstrap.css" rel="stylesheet">
     <link href="../css/bootstrap-icons.css" rel="stylesheet">
     <link href="../css/sidenav.css" rel="stylesheet">
@@ -18,8 +19,6 @@
     <link href="../css/index.css" rel="stylesheet">
     <link href="../css/profilpage.css" rel="stylesheet">
     <script src="../js/bootstrap.js"></script>
-    <script src="../js/plotly-2.18.2.min.js"></script>
-    <script src="../js/graph.js"></script>
 </head>
 
 <body>
@@ -27,10 +26,10 @@
         <div class="container-fluid col-11" id="header-container">
           <div class=" d-flex align-items-center justify-content-between">
             <div class="py-3 col-sm-auto justify-content-center">
-              <div id="title">JeuVente.fr</div>
+              <div id="title">JeuxVente.fr</div>
             </div>
-            <div class="dropdown text-end">
-              <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <div class="text-end">
+              <a href="profil.php" class="d-block link-dark text-decoration-none">
                 <i id="log-logo" class="bi bi-person-circle"></i>
               </a> 
             </div>
@@ -39,42 +38,8 @@
     </header>
     <div class="mt-3 container-fluid pb-3 flex-grow-1 d-flex flex-column flex-sm-row overflow-auto">
         <div class="row flex-grow-sm-1 flex-grow-0 container-fluid">
-            <aside class="col-sm-3 flex-grow-sm-1 flex-shrink-1 flex-grow-0 sticky-top pb-sm-0 pb-3 col-lg-2">
-                <div class="bg-light border rounded-3 p-1 h-100 sticky-top">
-                    <ul class="nav nav-pills flex-sm-column flex-row mb-auto justify-content-between text-truncate">
-                        <li class="my-1 nav-item">
-                            <a href="../../index.php" class="nav-link px-2 text-truncate">
-                                <i class="bi bi-house fs-5"></i>
-                                <span class="d-none d-sm-inline">Accueil</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="analyse.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-graph-up"></i></i>
-                                <span class="d-none d-sm-inline">Analyse</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="categorie.php" class="nav-link px-2 text-truncate">
-                            <i class="bi bi-layout-text-sidebar-reverse"></i></i>
-                                <span class="d-none d-sm-inline">Liste des categories</span>
-                            </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="produit.php" class="nav-link px-2 text-truncate"><i class="bi bi-card-text fs-5"></i>
-                                <span class="d-none d-sm-inline">Liste des produits</span> </a>
-                        </li>
-                        <li class="my-1">
-                            <a href="profil.php" class="nav-link px-2 text-truncate"><i class="bi bi-people fs-5"></i>
-                                <span class="d-none d-sm-inline">Profil</span> </a>
-                        </li>
-                        <a href="../../php/deconnexion.php" class="nav-link px-2 text-truncate">
-                        <i class="bi bi-toggle-off"></i></i>
-                            <span class="d-none d-sm-inline">Déconnexion</span>
-                        </a>
-                    </ul>
-                </div>
-            </aside>
+            <!-- BARRE DE NAVIGATION -->
+            <?php include 'barre_navigation_vd.php'; ?>
 
             <main class="col overflow-auto h-100">
                 <div class="bg-light border rounded-3 p-3">
