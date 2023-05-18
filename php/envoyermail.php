33231e54ec4b304cd0c789a2be824b93f0e1c41c
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-18 23:26:11 +0200

    Delete envoyermail.php

--- php/envoyermail.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-    require '../include/config.php';
-    $req = "UPDATE utilisateurs SET abonnement = 1 WHERE token='".$_SESSION['user']."'";
-    $stmt = $bdd->prepare($req);
-    $stmt->execute();
-    echo "Envoyer!";
-?>
