71abdd00f3b5dc87fac045ed78b57de887fe7a16
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-30 19:47:07 +0200

    Update co.php

--- php/co.php
+++ php/co.php
@@ -1,17 +1,63 @@
+<!-- SITE WEB 
+AÏT CHADI Anissa, BERGERE Sarah, COSTA Mathéo, FELGINES Sara
+ING 1 GI GROUPE 4 -->
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="NoS1gnal" />
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
     <link rel="stylesheet" type="text/css" href="../css/co.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Boogaloo&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/co.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <!-- INCLUSION ICONS -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Pacifico" />
+
     <title>Connexion</title>
 </head>
 
 <body>
-    <section>
+    <header class="container-fluid header">
+        <div class="container">
+            <a href="../index.php" class="logo">JeuxVentes.fr</a>
+            <div class="icons">
+                <?php 
+                // REDIRECTIONS PAGES/CHANGEMENT AFFICHAGE LORS DU CLIC SUR LOGO SELON LE PROFIL UTILISATEUR
+                if(!isset($_SESSION['statut'])){
+                    echo "<a href='php/panier.php' class='reseauxlog'><ion-icon name=cart></ion-icon> </a>";
+                    echo "<a href='./contact.php' class='reseauxlog'><ion-icon name=chatbubbles></ion-icon> </a>";
+                }else if($_SESSION['statut'] == "client"){
+                    echo "<a href='php/panier.php' class='reseauxlog'><ion-icon name=cart></ion-icon> </a>";
+                    echo "<a href='../profil/client/profil_cl.php' class='reseauxlog'><ion-icon name=person></ion-icon> </a>";
+                    echo "<a href='./contact.php' class='reseauxlog'><ion-icon name=chatbubbles></ion-icon> </a>";
+                }
+                else if ($_SESSION['statut'] == "admin"){
+                    echo "<a href='./gestion_messages.php' class='reseauxlog'><ion-icon name=mail-unread-outline></ion-icon> </a>";
+                    echo "<a href='../profil/admin/main_ad.php' class='reseauxlog'> <ion-icon name=flask-outline></ion-icon> </a>";
+                    echo "<a href='./deconnexion.php' class='reseauxlog'><ion-icon name=power-outline></ion-icon> </a>";
+                }else if($_SESSION['statut'] == "vendeur"){
+                    echo "<a href='./contact.php' class='reseauxlog'><ion-icon name=mail-unread-outline></ion-icon> </a>";
+                    echo "<a href='../profil/vendeur/main.php' class='reseauxlog'> <ion-icon name=storefront-outline></ion-icon> </a>";
+                    echo "<a href='./deconnexion.php' class='reseauxlog'><ion-icon name=power-outline></ion-icon> </a>";
+                }else if($_SESSION['statut'] == "livreur"){
+                    echo "<a href='./contact.php' class='reseauxlog'><ion-icon name=mail-unread-outline></ion-icon> </a>";
+                    echo "<a href='../profil/livreur/index2.php' class='reseauxlog'> <ion-icon name=bicycle-outline></ion-icon> </a>";
+                    echo "<a href='./deconnexion.php' class='reseauxlog'><ion-icon name=power-outline></ion-icon> </a>";
+                }
+                ?>
+            </div>
+        </div>
+    </header>
+    <!-- FIN DU HEADER -->
+    <main>
         <div class="form-box">
             <!--Pour afficher un message d'erreur--->
             <?php 
@@ -64,14 +110,12 @@ sinon affiche compte non existant
                 </div>
                 <p class="inscription">Je n'ai pas encore de <span>compte</span>. Je m'en <span><a
                             href="inscription.php">crée</span> un.</a></p>
-                <div class="form-group">
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                <button type="submit" class="btn btn-dark btn-block">Connexion</button>
+            </form>
         </div>
-        </form>
-        </div>
-    </section>
+    </main>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
