fba557e464ee3fb5a98506bc544f445667fc49b7
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-30 19:12:43 +0200

    Update abonnement.php

--- php/abonnement.php
+++ php/abonnement.php
@@ -16,10 +16,7 @@ ING 1 GI GROUPE 4 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- POLICE -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Boogaloo&display=swap" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="../css/abonnement.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
