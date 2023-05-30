1aa81816995ed57da8332e9e48bb66c7d313d7ec
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-05-31 00:40:56 +0200

    Update abonnement.php

--- php/abonnement.php
+++ php/abonnement.php
@@ -1,10 +1,7 @@
-<!-- SITE WEB 
-AÏT CHADI Anissa, BERGERE Sarah, COSTA Mathéo, FELGINES Sara
-ING 1 GI GROUPE 4 -->
-
 <?php
     session_start();
     $_SESSION['page_abo']=true;
+    
 ?>
 
 <!DOCTYPE html>
@@ -14,17 +11,12 @@ ING 1 GI GROUPE 4 -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- POLICE -->
-    <link href="https://fonts.googleapis.com/css2?family=Boogaloo&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="../css/abonnement.css">
+    <link href="https://fonts.googleapis.com/css2?family=Boogaloo&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-
     <!-- INCLUSION ICONS -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
     <title>Notre Abonnement</title>
 </head>
 
@@ -36,11 +28,9 @@ ING 1 GI GROUPE 4 -->
                 <?php 
                 // REDIRECTIONS PAGES/CHANGEMENT AFFICHAGE LORS DU CLIC SUR LOGO SELON LE PROFIL UTILISATEUR
                 if(!isset($_SESSION['statut'])){
-                    echo "<a href='php/panier.php' class='reseauxlog'><ion-icon name=cart></ion-icon> </a>";
                     echo "<a href='./co.php' class='reseauxlog'><ion-icon name=person></ion-icon> </a>";
                     echo "<a href='./contact.php' class='reseauxlog'><ion-icon name=chatbubbles></ion-icon> </a>";
                 }else if($_SESSION['statut'] == "client"){
-                    echo "<a href='php/panier.php' class='reseauxlog'><ion-icon name=cart></ion-icon> </a>";
                     echo "<a href='../profil/client/profil_cl.php' class='reseauxlog'><ion-icon name=person></ion-icon> </a>";
                     echo "<a href='./contact.php' class='reseauxlog'><ion-icon name=chatbubbles></ion-icon> </a>";
                 }
@@ -62,15 +52,14 @@ ING 1 GI GROUPE 4 -->
         </div>
     </header>
     <!-- FIN DU HEADER -->
-
-    <main>
-        <?php if(getAbonnement() == 0 || getAbonnement() == 2){
+    <div>
+    <?php if(getAbonnement() == 0 || getAbonnement() == 2){
         echo '<div class="description">
-        <p class="head">Notre abonnement</h1>
+        <p class="entete">Notre abonnement</h1>
         <p class="texte">Venez découvrir notre nouvel abonnement qui vous récompensera de votre fidélité avec de nombreux avantages ! </p>
     </div>
     <div class="abonnement">
-        <ul>
+        <ul class="list">
             <li class="titre">Abonnement Premium</li>
             <li class="couleur">9,99€ par an</li>
             <li>Accès à une réduction de 10% sur votre commande</li>
@@ -86,9 +75,8 @@ ING 1 GI GROUPE 4 -->
             <input type="button" class="desabonnement" onclick="location.href=\'fin_abo.php\'" value="Je me désabonne !"></input>' ;
         }
         ?>
-    </main>
+    </div>
 </body>
-
 </html>
 <?php 
 function getAbonnement(){
