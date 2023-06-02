5306ef4d71303dae2b8c11d84742bd7a67152ad7
Sarah <97475527+sarahbergere@users.noreply.github.com>
2023-06-02 22:45:17 +0200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,28 +1,14 @@
 <?php 
 //Connexion avec la base de données
-/*e-commerce est le nom de ma base de donnée, root -> utilisateur et root -> mp
-attention pour mac le mp est root mais pour les autre le nom reste le meme et mdp =" " 
+/*
+ATTENTION : CHANGER LE NOM D'UTILISATEUR ET LE MOT DE PASSE QUI SONT ICI "root"
 */
-if (gethostname()=="pc-d-ingenieur") {
-
     try{
-        $bdd = new PDO("mysql:host=localhost;dbname=ecom;charset=utf8", "7", "7");
+        $bdd = new PDO("mysql:host=localhost;dbname=ecom;charset=utf8", "root", "root");
     }
 //erreur : renvoyer message 
     catch(PDOException $e){
         die('Erreur : '.$e->getMessage());
     }
-}
-else {
-    
-    try{
-        $bdd = new PDO("mysql:host=localhost;dbname=ecom;charset=utf8", "sarah", "sarah");
-    }
-//erreur : renvoyer message 
-    catch(PDOException $e){
-        die('Erreur : '.$e->getMessage());
-    }
-};
-
 
 ?>
